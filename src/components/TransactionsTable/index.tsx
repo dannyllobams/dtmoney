4d042b2c212cc10551ce4ea0a9deb8a7d54e398f
Dannyllo Baltazar <dannyllo.souza@tsea.com.br>
2021-12-02 21:19:44 -0300

    Componente: transactions table

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data Transação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de WebSite</td>
+                        <td className="deposit">R$ 12,00</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Desenvolvimento de WebSite</td>
+                        <td className="withdraw">- R$ 12,00</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    )
+}
